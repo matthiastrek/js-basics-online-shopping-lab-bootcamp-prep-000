ddd5c9afabc994490fb317d495a2603d716ca73a
Matthew Trenk <matt.trenk@gmail.com>
2018-01-24 19:51:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    console.log('Sorry, we don\'t have crdit card on file for you.')
+    console.log('Sorry, we don\'t have a credit card on file for you.')
   } else {
-    console.log('Your total cost is $ ' + total() + ', which will be charged to the card ' + cardNumber + '.')
+    console.log('Your total cost is $' + total() + ', which will be charged to the card ' + cardNumber + '.')
     cart = []
   }
 }
