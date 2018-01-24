86fe3cb80468f2605d87d644115d9cf35a0f8018
Matthew Trenk <matt.trenk@gmail.com>
2018-01-24 19:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,10 @@ function removeFromCart(item) {
       itemInCart = true
       cart.splice(i, 1)
     }
-  }   
+  }
+  if (!itemInCart){
+    console.log('That item is not in your cart.')
+  }
   return cart
 }
 
