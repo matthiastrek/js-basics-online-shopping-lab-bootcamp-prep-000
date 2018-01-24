ef53b4a7d73e8d8d9203114e96eb44eca81c782b
Matthew Trenk <matt.trenk@gmail.com>
2018-01-24 19:25:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (cart[i].hasOwnProperty(item)) {
       itemInCart = true
-      itemInCar.splice
+      cart.splice(i, 1)
     } else 
   }
 }
