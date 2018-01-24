267391555f8ed9d9160033c2fc98d805445ca750
Matthew Trenk <matt.trenk@gmail.com>
2018-01-24 19:46:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,5 +68,6 @@ function placeOrder(cardNumber) {
     console.log('Sorry, we don\'t have crdit card on file for you.')
   } else {
     console.log('Your total cost is $ ' + total() + ', which will be charged to the card ' + cardNumber + '.')
+    cart = []
   }
 }
